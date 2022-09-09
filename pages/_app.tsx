361b469cc85a5fe88a9cdf3ecee60ff4887104d7
aryamankha <aryamank@princeton.edu>
2022-09-09 17:06:01 -0400

    bump to version 0.6.6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,11 @@ import Head from "next/head";
 import { NextSeo } from "next-seo";
 import { useEffect } from "react";
 
-if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
+if (
+  typeof window !== "undefined" &&
+  process.env.NODE_ENV === "development"
+  // && /VIVID_ENABLED=true/.test(document.cookie)
+) {
   import("vivid-studio").then((v) => v.run());
   import("vivid-studio/style");
 }
